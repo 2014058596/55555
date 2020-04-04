68c6ae6fa4fbd9f8ac9668dc3a7b639e21737549
55555 <2014058596@qq.com>
2020-04-04 19:14:16 +0800

    更新

---
+++ 55555-gateway/src/main/java/cn/com/code/gateway/GatewayStartApplication.java
@@ -0,0 +1,32 @@
+package cn.com.code.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @ClassName: AdminStartApplication
+ * @Description: TODO
+ * @author: 55555
+ * @date: 2020年04月04日 5:53 下午
+ */
+@SpringBootApplication
+@ComponentScan("cn.com.code")
+@EnableDiscoveryClient
+public class GatewayStartApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(GatewayStartApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayStartApplication.class, args);
+    }
+
+
+
+}
