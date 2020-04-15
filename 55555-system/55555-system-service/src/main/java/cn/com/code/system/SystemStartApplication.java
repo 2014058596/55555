15c04778fa4aabede7d1db2b4b77377f43c2deb4
韩玉坤 <hanyukun@geostar.com.cn>
2020-04-15 09:42:54 +0800

    更新

---
+++ 55555-system/55555-system-service/src/main/java/cn/com/code/system/SystemStartApplication.java
@@ -0,0 +1,10 @@
+package cn.com.code.system;
+
+/**
+ * @ClassName: SystemStartApplication
+ * @Description: TODO
+ * @author: 55555
+ * @date: 2020年04月15日 9:41 上午
+ */
+public class SystemStartApplication {
+}
