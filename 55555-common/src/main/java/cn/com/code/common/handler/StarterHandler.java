156328e420e14cd7368bae2dafac1114eb3b8a38
韩玉坤 <hanyukun@geostar.com.cn>
2020-04-22 00:49:30 +0800

    更新

--- 55555-common/src/main/java/cn/com/code/common/handler/StarterHandler.java
+++ 55555-common/src/main/java/cn/com/code/common/handler/StarterHandler.java
@@ -2,6 +2,7 @@ package cn.com.code.common.handler;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
 /**
  * @ClassName: StarterHandler
@@ -10,12 +11,13 @@ import org.springframework.boot.CommandLineRunner;
  * @date: 2020年04月04日 10:39 下午
  */
 @Log4j2
+@Component
 public class StarterHandler implements CommandLineRunner {
 
 
     @Override
     public void run(String... args) throws Exception {
 
-        log.debug("-----------系统系统成功-------------");
+        log.debug("-----------系统启动成功-------------");
     }
 }
